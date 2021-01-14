5856ab8ca76459ba6311d9dadb7684cbe99d8b0f
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-01-14 13:21:06 +0500

    Добавьте файлы проекта.

---
+++ ПомощникПовара/DBContext/DataBaseContext.cs
@@ -0,0 +1,28 @@
+﻿using _1CMurabbiy.Model.Documents;
+//using _1CMurabbiy.View.DocumentViews;
+using _1CMurabbiy.Model.SpecialDbModel;
+using Spravochniki;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using _1CMurabbiy.Model.Tablitsi;
+using _1CMurabbiy.Model.PlaniVidovRascheta;
+using _1CMurabbiy.Model.PlanSchetov;
+using _1CMurabbiy.Model.Spravochniki;
+using System.ComponentModel.DataAnnotations;
+using ПомощникПовара.Model;
+
+namespace _1CMurabbiy.DBContext
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("ChefHelperDB") 
+        {
+            Database.SetInitializer<DataBaseContext>(new CreateDatabaseIfNotExists<DataBaseContext>());
+             Configuration.AutoDetectChangesEnabled = true;
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Extra> Extras { get; set; }
+
+    }
+}
---
+++ ПомощникПовара/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПомощникПовара.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconSource { get; set; }
+    }
+}
